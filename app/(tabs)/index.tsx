e67d8fdbe889a50013d75f9c004ef1083f05f4a1
Nick <nhowell3@atu.edu>
2024-09-17 12:34:32 -0500

    Init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,112 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Button,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
 
 export default function HomeScreen() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
+  //setIsLogin(false);
+
+  function isStrValid(str: string){
+    if (str){
+      if (str != ''){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const handleSubmit = () => {
+    if (isStrValid(email) && isStrValid(password)){
+      if (isLogin){
+        setMessage("Login Attempted");
+        return;
+      }
+      if (isStrValid(firstName) && isStrValid(lastName)){
+        setMessage("Sign up successful");
+        return;
+      }
+    }
+    setMessage("Please fill out all fields.");
+  };
+
+  const handleSwitch = () => {
+    setIsLogin(!isLogin);
+    setMessage('');
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <SafeAreaView style={styles.container}>
+
+      {!isLogin &&
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="First Name"
+          onChangeText={text => setFirstName(text)}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <TextInput
+          style={styles.input}
+          placeholder="Last Name"
+          onChangeText={text => setLastName(text)}
+        />
+      </View>
+      }
+      <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={text => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={text => setPassword(text)}
+        />
+      <View style={styles.button}>
+        <Button title={isLogin ? "Login" : "Sign up"} onPress={handleSubmit} />
+      </View>
+      <View style={styles.button}>
+        <Button title={isLogin ? "SWITCH TO SIGN UP" : "SWITCH TO LOGIN"} onPress={handleSwitch}/>
+      </View>
+      {message && <Text style={styles.validationText}>{message}</Text>}
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input: {
+    height: 40,
+    borderWidth: 1,
+    width: '80%',
+    margin: 'auto',
+    marginBottom: 12,
+    marginTop: 12,
+    paddingLeft: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  button: {
+    width: '50%',
+    margin: 'auto',
+    marginTop: 12,
+    marginBottom: 12,
   },
+  validationText: {
+    textAlign: 'center',
+    fontSize: 18
+  }
 });
